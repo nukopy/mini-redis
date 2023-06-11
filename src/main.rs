280fd461abfc461a10350aa99923f01d348f1c7b
nukopy <nukopy@gmail.com>
2023-06-11 20:12:21 +0900

    chore: add sample test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    #[should_panic]
+    fn it_works2() {
+        panic!("Make this test fail");
+    }
+}
